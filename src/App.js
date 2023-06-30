ca3c4248a28cc0c79dea0830d7b2f979acd49a8e
Jeniffer Giraldo <tris@Jeniffers-MacBook-Air-2.local>
2023-06-30 17:27:07 -0400

    FIRST commit, currency converter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import './styles/App.css';
+
 
 function App() {
+  const [convertU, setConvertU] = useState("")
+  const [won, setWon] = useState()
+  const handleConversion = (e) => {
+    e.preventDefault()
+  
+  const usd = Number (e.target.usd.value);
+  const won = Number (e.target.won.value);
+
+  if (convertU !== "") {
+    const convertedWon = usd / 0.00078;
+    setWon(convertedWon);
+    } else {
+      const convertedUsd = won * 0.0078;
+      setConvertU(convertedUsd);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Currency Converter</h1>
+      <form onSubmit={handleConversion} >
+      
+      <p className="currency-name">USD</p>
+        
+        <input type="text" name="usd" placeholder="Enter USD Amount"
+          value={convertU} onChange={e => {
+            setConvertU(e.target.value)
+            setWon("")
+          }}
+        />
+
+        <p className="currency-name">WON</p>
+
+        <input type="text" name="won" placeholder="Enter Won Amount" 
+          value={won} onChange={e => {
+            setWon(e.target.value)
+            setConvertU("")
+          }}
+        />
+          <button type="submit" value="usd" >Convert</button>
+      </form>
+    </main>
   );
 }
-
+ 
 export default App;
